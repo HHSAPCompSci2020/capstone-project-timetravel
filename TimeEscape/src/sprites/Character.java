f5b09e612679e29f72110d2eeddaa0d9d5d17e12
Taya Williams <twilliams561@student.fuhsd.org>
2021-05-11 14:24:07 -0700

    created basic methods for character and sprite

--- TimeEscape/src/sprites/Character.java
+++ TimeEscape/src/sprites/Character.java
@@ -6,5 +6,49 @@ package sprites;
  */
 
 public class Character extends Sprite{
-
+	
+	public double height = 50.0;
+	public double width = 50.0;
+	public double vx, vy, gravity, friction;
+	public double jump;
+	
+	public Character(PImage img, double x, double y) {
+		super(img, x, y, width, height);
+		vx = 0;
+		vy = 0;
+		gravity = 0.2; 
+		friction = 0.95;
+		jump = 15;
+	}
+	
+	public void walk(int dir) {
+		vx += dir;
+	}
+	
+	public void jump() {
+		vy -= jump;
+	}
+	
+	public void stop() {
+		vx = 0;
+		vy = 0;
+	}
+	
+	public void act() {
+		
+		double x = s.getX();
+		x += vx;
+		s.setX(x);
+		
+		double y = s.getY();
+		y += vy;
+		s.setY(y);
+		
+		vy *= friction;
+		vx *= friction;
+		
+		vy += gravity;
+		
+		
+	}
 }
